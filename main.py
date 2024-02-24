85b6ab0b9da1b8b3f0116461de00f7c4693c6bac
MrDoge5310 <daniil_trotsenko@nure.ua>
2024-02-24 17:06:39 +0200

    1

--- main.py
+++ main.py
@@ -34,44 +34,66 @@ params = {
     "classifies": ["mass", "profession"]
 }
 
+params_ = {
+    "fiat": "UAH",
+    "page": 1,
+    "rows": 20,
+    "transAmount": user['minSingleTransAmount'],
+    "tradeType": "BUY",
+    "asset": "USDT",
+    "countries": [],
+    "proMerchantAds": False,
+    "shieldMerchantAds": False,
+    "publisherType": 'merchant',
+    "payTypes": ['Monobank', 'PUMB'],
+    "classifies": ["mass", "profession"]
+}
+
 client = Client('6HC8gJQtWG4UbEVb5usCD8EhuddBkqJ4XJD04JF4j4e03vbt9WMRP3PBXBcOgLoA',
                 'q1jfzQ77JvLtCmlMtBG7ynNySoz1sPO42sWTXQ3G9m4gncDiUg4bEll8vyJYpdG0')
 
 link = 'https://p2p.binance.com/bapi/c2c/v2/friendly/c2c/adv/search'
 
-competitor = None
+eth_competitor = None
+usdt_competitor = None
 rate = float(client.get_symbol_ticker(symbol='ETHUSDT')['price'])
 cur_clearance = None
 
-# for r in rates_response['data']:
-#     if r['pair'] == 'UAH_USDT':
-#         print(r['rate'])
+while True:
+    os.system('cls')
+    response = requests.post(link, headers=headers, json=params).json()
+    response_ = requests.post(link, headers=headers, json=params_).json()
+    advertisements = response['data']
+    advertisements_ = response_['data']
 
-# while True:
-#     os.system('cls')
-#     response = requests.post(link, headers=headers, json=params).json()
-#     advertisements = response['data']
-#
+    for adv in advertisements:
+        if (user['minSingleTransAmount'] <= float(adv['adv']['maxSingleTransAmount'])
+                and float(adv['adv']['maxSingleTransAmount']) - float(adv['adv']['minSingleTransAmount']) >= 5000
+                and adv['advertiser']['userNo'] != user['userNo']):
+            eth_competitor = adv
+            break
+        else:
+            pass
 
+    for adv in advertisements_:
+        if (user['minSingleTransAmount'] <= float(adv['adv']['maxSingleTransAmount'])
+                and float(adv['adv']['maxSingleTransAmount']) - float(adv['adv']['minSingleTransAmount']) >= 5000
+                and adv['advertiser']['userNo'] != user['userNo']):
+            usdt_competitor = adv
+            break
+        else:
+            pass
 
+    nickname = eth_competitor['advertiser']['nickName']
+    buy_eth_price = float(eth_competitor['adv']['price']) - 1
+    buy_usdt_price = float(usdt_competitor['adv']['price']) - 0.01
+    cur_clearance = buy_eth_price / rate - buy_usdt_price
 
+    print(f"Конкурент --> {nickname} торгует по цене {buy_eth_price + 1}")
+    print(f"Можно стать по цене {float(buy_eth_price)}")
+    print(f"Зазор - {cur_clearance}")
+    if cur_clearance < user['min_clearance']:
+        print(f"Торговать невыгодно, зазор меньше {user['min_clearance']}")
+    print('-------------------------------------------')
 
-    # for adv in advertisements:
-    #     if (user['minSingleTransAmount'] <= float(adv['adv']['maxSingleTransAmount'])
-    #             and float(adv['adv']['maxSingleTransAmount']) - float(adv['adv']['minSingleTransAmount']) >= 5000
-    #             and adv['advertiser']['userNo'] != user['userNo']):
-    #         # cur_clearance = float(adv['adv']['price']) - 1 / rate
-    #         competitor = adv
-    #         break
-    #     else:
-    #         pass
-    #
-    # min_price = competitor['adv']['price']
-    # nickname = competitor['advertiser']['nickName']
-    #
-    # print(f"Конкурент --> {nickname} торгует по цене {min_price}")
-    # print(f"Можно стать по цене {float(min_price) - 1}")
-    # print(f"Зазор - {cur_clearance}")
-    # print('-------------------------------------------')
-    #
-    # time.sleep(1)
+    time.sleep(1)
